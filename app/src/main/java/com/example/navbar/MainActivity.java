6522cfb29697e4e6c549e9c5898fd46b46109ddc
suki119 <it20045326@my.sliit.lk>
2021-09-23 18:59:28 +0530

    aded admin_login/register

--- app/src/main/java/com/example/navbar/MainActivity.java
+++ app/src/main/java/com/example/navbar/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.navbar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.navbar.admin.admin_login;
+
 public class MainActivity extends AppCompatActivity {
 
     //initialize variables
 //    DrawerLayout drawerLayout;
-    Button button,adminlogin;
+    Button button,adminloginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.dashboard);
-        adminlogin=findViewById(R.id.adminlogin);
+        adminloginbtn=findViewById(R.id.adminlogin_btn);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,10 +32,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        adminlogin.setOnClickListener(new View.OnClickListener() {
+        adminloginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
+                Intent intent = new Intent(MainActivity.this, admin_login.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/navbar/admin/admin_login.java
@@ -0,0 +1,98 @@
+package com.example.navbar.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.navbar.Data.DatabaseHelper;
+import com.example.navbar.Model.User;
+import com.example.navbar.R;
+
+
+public class admin_login extends AppCompatActivity {
+
+    EditText editTextEmail, editTextPassword;
+    Button buttonLogin;
+    TextView textViewRegister,textforgetpassword;
+    DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        buttonLogin = findViewById(R.id.buttonLogin);
+
+
+
+
+
+
+        textforgetpassword = findViewById(R.id.text_forgetpassword);
+        textforgetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(admin_login.this, RegisterActivity.class));
+                String password = editTextEmail.getText().toString().trim();
+            }
+        });
+
+
+
+
+
+
+
+        textViewRegister = findViewById(R.id.textViewRegister);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(admin_login.this, RegisterActivity.class));
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+
+
+                Boolean res = db.checkUser(email, password);
+                User user = new User();
+                Log.d("IDdetails", String.valueOf(user.getId()));
+
+
+                if (res == true) {
+                    Intent HomePage = new Intent(admin_login.this, HomeActivity.class);
+                    Bundle b = new Bundle();
+                    b.putString("textViewEmail", editTextEmail.getText().toString());
+                    b.putString("textViewPassword", editTextPassword.getText().toString());
+
+                    String y = db.selectOneUserSendUserName(email, password);
+                    int x = db.selectOneUserSendId(email,password);
+                    Log.d("TAG" , "ID =  " + x);
+
+                    b.putString("textViewUsername",y);
+                    b.putString("textViewId", String.valueOf(x));
+
+                    HomePage.putExtras(b);
+                    startActivity(HomePage);
+                } else {
+                    Toast.makeText(admin_login.this, "Login Error", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
