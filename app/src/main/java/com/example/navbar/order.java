6579998316382b27b20dd7980fdc7fcc77394edf
tharaka dayan <it20055530@my.sliit.lk>
2021-09-25 16:14:42 +0530

    'complete_Store_Insert'

---
+++ app/src/main/java/com/example/navbar/order.java
@@ -0,0 +1,51 @@
+package com.example.navbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class order extends AppCompatActivity {
+
+    EditText NIC_Input, FirstName_Input, LastName_Input, StreetAddress_Input, City_Input,
+            Email_Input, PhoneNum_Input, Quantity_Input;
+    Button Confirm_Button;
+
+    String nic,firstname,lastname,streetAddress,city,email,phoneNum,quantity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        NIC_Input = findViewById(R.id.NIC_Input);
+        FirstName_Input = findViewById(R.id.FirstName_Input);
+        LastName_Input = findViewById(R.id.LastName_Input);
+        StreetAddress_Input = findViewById(R.id.StreetAddress_Input);
+        City_Input = findViewById(R.id.City_Input);
+        Email_Input = findViewById(R.id.Email_Input);
+        PhoneNum_Input = findViewById(R.id.PhoneNum_Input);
+        Quantity_Input = findViewById(R.id.Quantity_Input);
+        Confirm_Button = findViewById(R.id.Confirm_Button);
+
+        Confirm_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                nic = NIC_Input.getText().toString().trim();
+                firstname = FirstName_Input.getText().toString().trim();
+                lastname = LastName_Input.getText().toString().trim();
+                streetAddress = StreetAddress_Input.getText().toString().trim();
+                city = City_Input.getText().toString().trim();
+                email =  Email_Input.getText().toString().trim();
+                phoneNum = PhoneNum_Input.getText().toString().trim();
+                quantity = Quantity_Input.getText().toString().trim();
+
+                OrderDatabaseHelper storeDB = new OrderDatabaseHelper(order.this);
+                storeDB.addOrder(nic,firstname,lastname,streetAddress,city,email,phoneNum,quantity);
+            }
+        });
+    }
+}
