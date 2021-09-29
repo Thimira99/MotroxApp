2e0f9d41522aff5fb61beddab44d0a63f9de64c2
suki119 <it20045326@my.sliit.lk>
2021-09-29 23:16:28 +0530

    add_practice_test

--- app/src/main/java/com/example/navbar/MainActivity.java
+++ app/src/main/java/com/example/navbar/MainActivity.java
@@ -10,27 +10,24 @@ import android.widget.Button;
 import com.example.navbar.admin.RegisterActivity;
 import com.example.navbar.admin.admin_login;
 
-public class MainActivity extends AppCompatActivity {
 
-    //initialize variables
-//    DrawerLayout drawerLayout;
-    Button button,adminloginbtn;
+public class MainActivity extends AppCompatActivity{
+
+
+
+    Button adminloginbtn,customerloginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.dashboard);
-        adminloginbtn=findViewById(R.id.adminlogin_btn);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,Dashboard.class);
-                startActivity(intent);
-            }
-        });
+        adminloginbtn = findViewById(R.id.adminlogin_btn);
+        customerloginbtn = findViewById(R.id.customerlogin_btn);
+
+
+
 
         adminloginbtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,83 +37,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //assign variable
-//        drawerLayout = findViewById(R.id.drawer_layout);
-    }
-
-//    public void ClickMenu(View view){
-//        //open drawer
-//        openDrawer(drawerLayout);
-//    }
-//
-//    public static void openDrawer(DrawerLayout drawerLayout) {
-//        //open drawer layout
-//        drawerLayout.openDrawer(GravityCompat.START);
-//    }
-//
-//    public void ClickLogo(){
-//        //close drawer
-//        closeDrawer(drawerLayout);
-//    }
-//
-//    public static void closeDrawer(DrawerLayout drawerLayout) {
-//        //close drawer layout
-//        //check conditions
-//        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
-//            //when drawer is open
-//            //close drawer
-//            drawerLayout.closeDrawer(GravityCompat.START);
-//        }
-//    }
-//
-//    public static void redirectActivity(Activity activity,Class aClass) {
-//        //initialize intent
-//        Intent intent = new Intent(activity,aClass);
-//
-//        //set flag
-//        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//
-//        //start activity
-//        activity.startActivity(intent);
-//    }
-//
-//    public void ClickProfile(View view){
-//        //Redirect activity
-//        redirectActivity(this,Profile.class);
-//
-//    }
-//
-//    public void ClickDashboard(View view){
-//        //recreate activity
-//        recreate();
-//    }
-//
-//    public void ClickStore(View view){
-//        //Redirect activity
-//        redirectActivity(this,Store.class);
-//    }
-//
-//    public void ClickService(View view){
-//        //Redirect activity
-//        redirectActivity(this, Service.class);
-//    }
-//
-//    public void ClickAboutUs(View view){
-//        //Redirect activity
-//        redirectActivity(this,AboutUs.class);
-//    }
-//
-//    public void ClickRateUs(View view){
-//        //Redirect activity
-//        redirectActivity(this,RateUs.class);
-//    }
-//
-//    @Override
-//    protected void onPause() {
-//        super.onPause();
-//        //close drawer
-//        closeDrawer(drawerLayout);
-//    }
 
 
+    };
 }
